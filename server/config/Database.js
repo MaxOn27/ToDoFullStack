e529928b0fd92c5adddbf4cb46f4237c868d30df
Max Tyzhnenko <maxym271212@gmail.com>
2022-11-27 22:42:07 +0200

    added sequelize

--- server/config/Database.js
+++ server/config/Database.js
@@ -2,8 +2,8 @@ import {Sequelize} from "sequelize";
 
 const db = new Sequelize(
     "todo_db",
-    "MaxUser",
-    "MyMySQL@271212!",
+    "",
+    "",
     {host: "localhost", dialect: "mysql"}
 );
 
